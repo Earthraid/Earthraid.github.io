7ec8667ac99ff7622ee6aa54a19d1263da355c5c
Jerrad Christian <jerradchristian@yahoo.com>
2018-03-10 21:07:41 -0500

    First time push to git

--- PersonalSite/PersonnalSite/Global.asax.cs
+++ PersonalSite/PersonnalSite/Global.asax.cs
@@ -5,11 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Diagnostics;
 
 namespace PersonnalSite
 {
+
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception objErr = Server.GetLastError().GetBaseException();
+            string err = "Error Caught in Application_Error event\n" +
+            "Error in: " + Request.Url.ToString() +
+            "\nError Message:" + objErr.Message.ToString() +
+            "\nStack Trace:" + objErr.StackTrace.ToString();
+            EventLog.WriteEntry("Sample_WebApp", err, EventLogEntryType.Error);
+            Server.ClearError();
+            //additional actions...
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
--- PersonalSite/PersonnalSite/Models/LoginViewModel.cs
+++ PersonalSite/PersonnalSite/Models/LoginViewModel.cs
@@ -8,12 +8,13 @@ namespace Validation.Web.Models
 {
     public class LoginViewModel
     {
+        //Annotation for server side validation
         [Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required]
-        [MinLength(8, ErrorMessage = "Password must be 8 characters or more")]
+        [MinLength(8)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
