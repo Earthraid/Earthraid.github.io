cce987ad61495ba0b83cb6a24356840b6bb9875d
Jerrad Christian <jerradchristian@yahoo.com>
2018-03-20 21:00:41 -0400

    shadow front page jumbotron

---
+++ PersonnalSite/Content/JavaScript.js
@@ -0,0 +1,11 @@
+﻿$(document).ready
+    (function () {
+        $("#moves").on({
+            mouseover: function () {
+                $(this).css({
+                    left: (Math.random() * 200) + "px",
+                    top: (Math.random() * 200) + "px",
+                });
+            }
+        });
+    });
